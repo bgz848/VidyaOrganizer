2657cbac621bfa09b057aa8da42e3291394f36f4
bgz848 <bgz848@myy.haaga-helia.fi>
2025-04-24 22:08:47 +0300

    Added basic Light/Dark mode support.

--- Style.js
+++ Style.js
@@ -1,4 +1,8 @@
-export const colors = {
+import { Appearance } from 'react-native';
+
+const colorScheme = Appearance.getColorScheme();
+
+export const darkColors = {
   background: '#1e1e1e',
   text: '#fff',
   secondaryText: '#aaa',
@@ -10,5 +14,20 @@ export const colors = {
   buttonDelete: '#f44336',
   selectedItem: '#00f'
 };
+
+const lightColors = {
+  background: '#ffffff',
+  text: '#000000',
+  secondaryText: '#555555',
+  secondaryBackground: '#f2f2f2',
+  tertiaryBackground: '#e0e0e0',
+  button: '#4CAF50',
+  buttonConfirm: '#4CAF50',
+  buttonDefault: '#2196F3',
+  buttonDelete: '#f44336',
+  selectedItem: '#00f'
+};
+
+export const colors = colorScheme === 'dark' ? darkColors : lightColors;
 
 export default colors;
